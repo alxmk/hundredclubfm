611744e085258bb748fa8bea03dd3453885bd36f
Alex Kelly <alex.kelly@william-lee.com>
2011-05-20 11:43:40 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Football Manager 100-Club Generator
+
+This is a simple web app to generate a 100-club code listing for each season in a Football Manager career game, created by [SnakeXe](mailto:alex.kelly@william-lee.com).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hundredclubfm::Application.config.secret_token = '57edc44352e5f2eff3e978a51944b0d8b276d976f7a5c07c19d4b95656e93cd0b2d348eca292d79bb0a27cd35815bc1311c70fc0224846a208b45e798c4a7489'
